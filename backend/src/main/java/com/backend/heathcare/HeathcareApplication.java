87d917949fa81e26366dde62a4dc2c65a7f859df
diethon <vietthde180482@fpt.edu.vn>
2025-06-15 21:04:06 +0700

    first commit

---
+++ backend/src/main/java/com/backend/heathcare/HeathcareApplication.java
@@ -0,0 +1,13 @@
+package com.backend.heathcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeathcareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeathcareApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/backend/heathcare/HeathcareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.heathcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeathcareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
